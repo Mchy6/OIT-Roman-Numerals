b2a5ee38182516d5f4c6e6e60ebc0b07e87811c9
Matthew Bailey <mmchy6@gmail.com>
2024-04-05 12:02:29 -0600

    checks for repeated values

--- r_numeral_functions.py
+++ r_numeral_functions.py
@@ -1,17 +1,40 @@
 def check_input(r_numeral) -> tuple:
-    
-    roman_numerals = "IVXLCDM"
 
     r_numeral_upper = r_numeral.upper()
+
+    # Check if all the characters are valid Roman numerals
+    roman_numerals = "IVXLCDM"
 
     for i in range(len(r_numeral_upper)):
         if r_numeral_upper[i] not in roman_numerals:
             return False, r_numeral[i] + " is not a valid character."
         
+    # Check if the string is empty
     if len(r_numeral_upper) == 0:
         return False, "Empty string is not a valid Roman numeral."
         
     i_list: list[int] = _convert_to_int_list(r_numeral_upper)
+
+    repeated: int = 1
+    for i in range(len(i_list) - 1):
+
+        # Increment repeated if the current character is the same as the next character
+        if i < (len(i_list) - 1) and i_list[i] == i_list[i + 1]:
+            repeated += 1
+        else:
+            repeated = 1
+            
+        if i_list[i] % 2 == 0:
+            # Check if current character is repeated more than three times
+            if repeated > 3:
+                return False, r_numeral_upper[i] + " cannot be repeated more than three times."
+            
+        else:
+            # Check if current character is repeated more than once
+            if repeated > 1:
+                return False, r_numeral_upper[i] + " cannot be repeated more than once."
+
+    return True, ""
 
 def rn_to_decimal(r_numeral) -> int:
     ...
