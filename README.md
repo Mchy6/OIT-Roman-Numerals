84e7668099590b5c79ed2a3ed9aaa55750fba471
Matthew Bailey <mmchy6@gmail.com>
2024-04-05 11:29:12 -0600

    _convert_to_int_list, check for valid character, check for empty string

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # OIT-Roman-Numerals
 OIT Code Challenge: Decimal to Roman Numeral Converter
 
+## Process
+### Structure
+* I want there to be an easy way to use the program, so it's been clear from the start that I want to have a "main" file to run and a seperate file that includes the funcitons
+* After some deliberation, I've decided to use 3 different functions in my r_numeral_functions.py file
+* check_input and rn_to_decimal could probably be combined into one function with slightly improved efficiency, but that would violate single-responsibility rule 
+
+## Handwritten Notes
--- r_numeral_functions.py
+++ r_numeral_functions.py
@@ -1,9 +1,38 @@
 def check_input(r_numeral) -> tuple:
-    ...
+    
+    roman_numerals = "IVXLCDM"
+
+    r_numeral_upper = r_numeral.upper()
+
+    for i in range(len(r_numeral_upper)):
+        if r_numeral_upper[i] not in roman_numerals:
+            return False, r_numeral[i] + " is not a valid character."
+        
+    if len(r_numeral_upper) == 0:
+        return False, "Empty string is not a valid Roman numeral."
+        
+    i_list: list[int] = _convert_to_int_list(r_numeral_upper)
 
 def rn_to_decimal(r_numeral) -> int:
     ...
 
 # Expects a valid Roman numeral uppercase string
 def _convert_to_int_list(r_numeral) -> list[int]:
-    ...
+    decimal_list = []
+    for char in r_numeral:
+        if char == "I":
+            decimal_list.append(0)
+        elif char == "V":
+            decimal_list.append(1)
+        elif char == "X":
+            decimal_list.append(2)
+        elif char == "L":
+            decimal_list.append(3)
+        elif char == "C":
+            decimal_list.append(4)
+        elif char == "D":
+            decimal_list.append(5)
+        elif char == "M":
+            decimal_list.append(6)
+
+    return decimal_list
